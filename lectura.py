d8b0872477708250813e3608070a52edeaa0bd7c
Lenovo-BKP <harcot.sys@gmail.com>
2022-10-19 09:04:08 -0500

    moving arduino filles into SerialEvent

--- lectura.py
+++ lectura.py
@@ -14,20 +14,20 @@ def read_tibbo():
     time.sleep(0.1)
     #telnetObj.write(b"exit\n")
     print(peso)
-    socket.send_fds(peso)
+#    socket.send_fds(peso)
     
 #def transmit_telnet(peso):
-    TCP_IP = '127.0.0.1'
-    TCP_PORT = 9091
-    BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((TCP_IP, TCP_PORT))
-    s.listen(1)
+#    TCP_IP = '127.0.0.1'
+#    TCP_PORT = 9091
+#    BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
+#    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#    s.bind((TCP_IP, TCP_PORT))
+#    s.listen(1)
 
-    conn, addr = s.accept()
-    while 1:
-        conn.send(peso)  # echo
-    conn.close()
+#    conn, addr = s.accept()
+#    while 1:
+#        conn.send(peso)  # echo
+#    conn.close()
 
 def run():
     while 1:
