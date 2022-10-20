3baed07fac24f71fd344144f43a1a2ebf4f50233
Lenovo-BKP <harcot.sys@gmail.com>
2022-10-20 13:11:22 -0500

    Pruebas para abrir dos telnet: NO hay exito

--- srv_telnet.py
+++ srv_telnet.py
@@ -1,10 +1,10 @@
-import sys
+#import sys
 import socket
-import telnetlib
+#import telnetlib
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
-BUFFER_SIZE = 10  # Normally 1024, but we want fast response
+BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
@@ -14,8 +14,12 @@ conn, addr = s.accept()
 print ('Connection address:', addr)
 while 1:
     #data = conn.recv(BUFFER_SIZE)
-    data = b"character_/n"
-    if not data: break
-    print ("received data:", data)
-    conn.send(data)  # echo
+    data1 = b"character_\n"
+    data2 = b"space_\n"
+    if not data1: break
+    if not data2: break
+    print ("received data:", data1)
+    print ("received data:", data2)
+    conn.send(data1)  # echo
+    conn.send(data2)  # echo
 conn.close()
